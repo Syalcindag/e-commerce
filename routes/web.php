ef62e1bc379656e2ec31b62b7051328530a474c1
LAPTOP-5OE16PM3\sefay <sefayalcindag1741@gmail.com>
2021-01-31 18:14:32 +0300

    created page template route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+
+Route::view('/category', 'category');
+Route::view('/cart', 'cart');
+Route::view('/product-detail', 'product-detail');
