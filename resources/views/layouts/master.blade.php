927c7344eff8171132ef43527f796832bd659b59
LAPTOP-5OE16PM3\sefay <sefayalcindag1741@gmail.com>
2021-02-07 22:06:49 +0300

    updated master.blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ config('app.locale') }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', config('app.name'))</title>
+    @include('layouts.partials.head')
+    @yield('head')
 </head>
-<body>
 
-    <a href="/">Home Page</a>
-    <a href="/category">Category Page</a>
-    <a href="/product-detail">Product Detail Page</a>
-    <a href="/cart">Cart Page</a>
-
-    <hr>
+<body id="commerce">
+    @include('layouts.partials.navbar')
+   
 
     @yield('content')
+
+    @include('layouts.partials.footer')
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    @yield('footer')
+
 </body>
+
 </html>
